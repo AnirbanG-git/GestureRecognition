1b0b23a42a2939c114e2bbf345fd0aa473a4a6a4
Anirban Gangopadhyay <anir.dr@gmail.com>
2024-06-30 19:52:58 +0000

    first submission with conv3d models

---
+++ model_2.py
@@ -0,0 +1,48 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv3D, Activation, BatchNormalization, MaxPooling3D, Dropout, Flatten, Dense
+from tensorflow.keras.optimizers import Adam
+
+def build_model(input_shape, num_labels, learning_rate):
+        model = Sequential()
+        model.add(Conv3D(16, (3, 3, 3), padding='same',
+                 input_shape=input_shape))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+
+        model.add(Conv3D(32, (3, 3, 3), padding='same'))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+
+        model.add(Conv3D(64, (3, 3, 3), padding='same'))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+        
+        model.add(Conv3D(128, (3, 3, 3), padding='same'))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        
+        model.add(Dense(128,activation='relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        model.add(Dense(128,activation='relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.25))
+        
+        model.add(Dense(num_labels,activation='softmax'))
+
+        model = model
+        
+        optimiser = Adam(learning_rate=learning_rate)
+
+        model.compile(optimizer=optimiser, loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+        print(model.summary())     
+        
+        return model
